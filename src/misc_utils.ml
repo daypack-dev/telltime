e9bb3ee523cf96b3c7e492c6a6b9ca95a14283c4
Darren Ldl <darrenldldev@gmail.com>
2020-09-18 19:12:12 +1000

    Updated use of daypack-lib

--- src/misc_utils.ml
+++ src/misc_utils.ml
@@ -1,8 +1,4 @@
 let make_search_param ~tz_offset_s ~search_years_ahead ~from_unix_second =
-  let open Daypack_lib in
-  Search_param.Years_ahead_start_unix_second
-    {
-      search_using_tz_offset_s = Some tz_offset_s;
-      start = from_unix_second;
-      search_years_ahead;
-    }
+  Daypack_lib.Search_param.make_using_years_ahead
+    ~search_using_tz_offset_s:tz_offset_s ~start:(`Unix_second from_unix_second)
+    search_years_ahead
