44397926939dfc27da600903a471219cd1ff1c47
Darren Ldl <darrenldldev@gmail.com>
2021-04-02 19:01:00 +1100

    Formatting

--- src/now_cmd.ml
+++ src/now_cmd.ml
@@ -1,7 +1,5 @@
 open Cmdliner
 
-let run () : unit =
-  Fmt.pr "%a\n"
-    ( Timere.pp_timestamp ()) Config.cur_timestamp
+let run () : unit = Fmt.pr "%a\n" (Timere.pp_timestamp ()) Config.cur_timestamp
 
 let cmd = (Term.(const run $ const ()), Term.info "now")
--- src/resolve_cmd.ml
+++ src/resolve_cmd.ml
@@ -42,44 +42,44 @@ let run (tz : string) (search_years_ahead : int) (time_slot_count : int)
     (format : string) (sep : string) (expr : string) : unit =
   match Timere.Time_zone.make tz with
   | None -> print_endline "Unrecognized time zone"
-  | Some tz ->
-  match Timere_parse.timere expr with
-  | Error msg -> print_endline msg
-  | Ok timere -> (
-      let search_end_exc =
-        Int64.add
-          Config.cur_timestamp
-          Timere.(Duration.make ~days:(search_years_ahead * 365) () |> Duration.to_seconds)
-      in
-      let timere =
-        let open Timere in
-        inter [timere; intervals [(Config.cur_timestamp, search_end_exc)]]
-      in
-      match Timere.resolve timere with
+  | Some tz -> (
+      match Timere_parse.timere expr with
       | Error msg -> print_endline msg
-      | Ok s -> (
-          Fmt.pr
-            "Searching in time zone (seconds)            : %s\n"
-            (Timere.Time_zone.name tz);
-          Fmt.pr
-            "Search by default starts from (in above time zone) : %a\n"
-            ( Timere.pp_timestamp ~display_using_tz:tz ~format:Config.default_date_time_format_string ())
-            Config.cur_timestamp;
-          print_newline ();
-          match s () with
-          | Seq.Nil -> print_endline "No matching time slots"
-          | Seq.Cons _ ->
-            s
-            |> OSeq.take time_slot_count
-            |> OSeq.iteri (fun i ts ->
-                let s =
-                  Timere.string_of_interval
-                    ~display_using_tz:tz ~format ts
+      | Ok timere -> (
+          let search_end_exc =
+            Int64.add Config.cur_timestamp
+              Timere.(
+                Duration.make ~days:(search_years_ahead * 365) ()
+                |> Duration.to_seconds)
+          in
+          let timere =
+            let open Timere in
+            inter
+              [ timere; intervals [ (Config.cur_timestamp, search_end_exc) ] ]
+          in
+          match Timere.resolve timere with
+          | Error msg -> print_endline msg
+          | Ok s -> (
+              Fmt.pr "Searching in time zone (seconds)            : %s\n"
+                (Timere.Time_zone.name tz);
+              Fmt.pr "Search by default starts from (in above time zone) : %a\n"
+                (Timere.pp_timestamp ~display_using_tz:tz
+                   ~format:Config.default_date_time_format_string ())
+                Config.cur_timestamp;
+              print_newline ();
+              match s () with
+              | Seq.Nil -> print_endline "No matching time slots"
+              | Seq.Cons _ ->
+                s
+                |> OSeq.take time_slot_count
+                |> OSeq.iteri (fun i ts ->
+                    let s =
+                      Timere.string_of_interval ~display_using_tz:tz
+                        ~format ts
                     in
-                  if i = 0 then Printf.printf "%s" s
-                  else Printf.printf "%s%s" sep s
-              );
-            print_newline () ) )
+                    if i = 0 then Printf.printf "%s" s
+                    else Printf.printf "%s%s" sep s);
+                print_newline () ) ) )
 
 let cmd =
   ( (let open Term in
